301386dbb21eafd7b13f70e45d9f267cbcd43552
Marlyirala21 <100405994@alumnos.uc3m.es>
2021-12-05 00:30:10 +0100

    cambioss

--- lab4/src/App.js
+++ lab4/src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from "react";
-import TareaForm from "./componentes/TareaForm";
-import Tarea from "./componentes/Tarea";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { getDatabase, ref, set, get } from "firebase/database";
+import { getDatabase, ref, set, get, push, remove, onChildAdded, query, onValue, startAt, orderByChild, update, equalTo, limitToFirst, orderByKey} from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,7 +14,7 @@ const firebaseConfig = {
   storageBucket: "lab4-100405994.appspot.com",
   messagingSenderId: "1060798621624",
   appId: "1:1060798621624:web:ad6a5d4de5af959719514d",
-  databaseURL:"https://lab4-100405994-default-rtdb.europe-west1.firebasedatabase.app/"
+  databaseURL: "https://lab4-100405994-default-rtdb.europe-west1.firebasedatabase.app/"
 
 };
 
@@ -30,13 +28,34 @@ const googleProvider = new GoogleAuthProvider();
 console.log(app);
 
 function App() {
-  const [isLoggedIn, setLoggedIn] = useState(false);
-  const [userName, setUserName] = useState(null);
 
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isDone, setDone] = useState(false);
+  const [userName, setUserName] = useState(null);
+  const ulTaskList = document.querySelector("#ulTaskList");
+
+  const uid = useRef(null);
+  const inputRef = useRef("");
+  const inputRefFilter = useRef("");
+
+
+  const [taskList, setTaskList] = useState([]);
+  let timer;
+
+  let start_x = 0;
+  let end_x = 0;
+  let prev_x = 0;
+  let start_time = 0;
+  const TIME_THRESHOLD = 500;
+  const SPACE_THRESHOLD = 200;
+
+  //autenticación con google 
   const signInWithGoogle = async () => {
     try {
       const res = await signInWithPopup(auth, googleProvider);
       const user = res.user;
+
+      uid.current = user.uid;
 
       const userRef = ref(db, `/users/${user.uid}`);
       const snapshot = await get(userRef);
@@ -46,7 +65,7 @@ function App() {
       if (data) {
         console.log(data);
       } else {
-        await set(ref(db, "users/" + user.uid),{
+        await set(ref(db, "users/" + user.uid), {
           username: user.displayName,
           email: user.email
         });
@@ -58,29 +77,123 @@ function App() {
       alert(err.message);
     }
   };
-  // const [listaTareas, setListaTareas] = useState([]);
 
-  // const nuevaTarea = (tarea) => {
-  //   setListaTareas([tarea, ...listaTareas]);
-  // };
+//cada vez que un usuario se registre, recuperamos la lista de tareas de ese usuario
+  useEffect(() => {
+    if (isLoggedIn) {
+      const tasksRef = ref(db, "tasks/" + uid.current);
+      //cuando se detecta que se añade una nueva tarea, lo añadimos a nuestra lista de tareas
+      onChildAdded(tasksRef, (data) => {
+        console.log("data.key", data.key);
+        console.log("data.val", data.val);
+        setTaskList((oldList) => [
+          ...oldList,
+          { val: data.val(), key: data.key }
+        ]);
+      });
+    }
+  }, [isLoggedIn]);
 
-  // const borrar = (id) => {
-  //   const listaFiltrada = listaTareas.filter((e, index) => index !== id);
-  //   setListaTareas(listaFiltrada);
-  // };
+  // useEffect(() =>{
+  //    //long touch
+  //   ulTaskList.addEventListener('touchstart', () => { 
+  //     timer = setTimeout(() => {
+  //       timer = null;
+  //       console.log("completado");
+  //       // handleTaskDone()
+  //     }, 2000);
+  //   });
+
+  //   function cancel() {
+  //     clearTimeout(timer);
+  //   }
+
+  // ulTaskList.addEventListener('touchend', cancel);
+  // ulTaskList.addEventListener('touchmove', cancel);
+  // }, [isLoggedIn])
+
+//damos el valor del input a la referencia que hemos creado para mantener este valor entre renderizaciones
+  function handleOnInput(e) {
+    inputRef.current = e.target.value;
+  }
+  
+//añadimos la tarea para el usuario en concreto
+  function handleAddTask() {
+      const taskListRef = ref(db, "tasks/" + uid.current);
+      const newTaskRef = push(taskListRef, { title: inputRef.current, done: isDone });
+    }
+
+//eliminamos la tarea 
+  function handleRemoveTask(pos, key) {
+    console.log("Remove", key);
+    const taskRef = ref(db, "tasks/" + uid.current + "/" + key);
+    remove(taskRef);
+    setTaskList((oldList) => {
+      console.log("oldlist", oldList);
+      const newList = [...oldList];
+      newList.splice(pos, 1);
+      console.log("newList", newList);
+      return newList;
+    });
+  }
+
+//marcamos la tarea como resuelta
+  function handleTaskDone (i, key){
+    const taskDoneRef = ref(db, "tasks/" + uid.current + "/" + key);
+    console.log(taskDoneRef);
+    setDone(true);
+    update(taskDoneRef, { title: inputRef.current, done: isDone })
+    setTaskList((oldList) => {
+      console.log(oldList);
+      const newList = [...oldList];
+      console.log("newlist", newList);
+      newList[i].val.done = true;
+      return newList;
+    }); 
+  }
+
+  function filter() {
+    const titlesRef = query(ref(db, "tasks/" + uid.current), orderByChild("title"), equalTo("d"));
+    onValue(titlesRef, (snapshot) => {
+      snapshot.forEach((el) => {
+        console.log("on value", el.val());
+      });
+    });
+  }
+     
 
   return (
     <div className="App">
-      {/* <TareaForm nuevaTarea={nuevaTarea} />
-      <div className="lista">
-        {listaTareas.map((e, index) => (
-          <Tarea tarea={e} borrar={borrar} id={index}/>
-        ))}
-      </div> */}
-      {isLoggedIn && <h1>Welcome, {userName}</h1>}
-      {!isLoggedIn && (
-        <button onClick={signInWithGoogle}>Sign-in with Google</button>
-      )}
+      {
+        (isLoggedIn) ?
+          (<div>
+            <h1>Welcome, {userName} 🙋🏻‍♀️</h1>
+            <input onInput={handleOnInput}></input>
+            <button onClick={handleAddTask}>añadir tarea</button>
+            <button onClick={filter}>añadir tarea</button>
+            
+        
+
+            <ul id={ulTaskList}>
+              {taskList.map((el, i) => {
+                return (
+                  <li className={el.val.done ? "done" : ""} key={el.key} >
+                    {el.val.title}
+                    {console.log("elemento", el)}
+                    <button onClick={() => handleRemoveTask(i, el.key)}>
+                      remove
+                    </button>
+                    <button onClick={() => handleTaskDone(i, el.key)}>done</button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          )
+          : (
+            <button onClick={signInWithGoogle}>Sign-in with Google</button>)
+
+      }
     </div>
   );
 }
